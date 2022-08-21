3113241ab455cd2acc3c90d47bfd2b8889607728
igorFilipenco <filipenko-igor@mail.ru>
2022-08-21 22:10:05 +0300

    master-init-commit-with-config

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spring-boot-2-sandbox
+
+ repository to play with Spring and tools during reading 'Spring Boot 2: best practices for professionals'
---
+++ src/main/java/com/springboot2/Springboot2Application.java
@@ -0,0 +1,14 @@
+package com.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class Springboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2Application.class, args);
+	}
+
+}
