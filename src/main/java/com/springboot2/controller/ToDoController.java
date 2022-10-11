02cb97b7ec8e081d8fd9df98245a7292a447ff15
igorFilipenco <filipenko-igor@mail.ru>
2022-10-11 23:55:28 +0300

    master-reactive-chapter

--- src/main/java/com/springboot2/controller/ToDoController.java
+++ src/main/java/com/springboot2/controller/ToDoController.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 import java.util.Objects;
@@ -23,62 +25,55 @@ public class ToDoController {
 
     @GetMapping(value = "/to-do", produces = {
             MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_XML_VALUE})
-    public ResponseEntity<Iterable<ToDo>> getToDos(@RequestHeader HttpHeaders headers) {
-        return new ResponseEntity<>(repository.findAll(), headers, HttpStatus.OK);
+    public ResponseEntity<Flux<ToDo>> getToDos(@RequestHeader HttpHeaders headers) {
+        return ResponseEntity.ok().body(repository.findAll());
     }
 
     @GetMapping(value = "/to-do/{toDoId}", produces = {
             MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_XML_VALUE})
-    public ResponseEntity<?> getToDo(@PathVariable String toDoId) {
-        Optional<ToDo> existingToDo = repository.findById(toDoId);
-
-        if (existingToDo.isEmpty()) {
-            return new ResponseEntity<>("To do with id" + toDoId + " does not exist", HttpStatus.NOT_FOUND);
-        }
-
-        return new ResponseEntity<>(repository.findById(toDoId).get(), HttpStatus.OK);
+    public Mono<ResponseEntity<ToDo>> getToDo(@PathVariable String toDoId) {
+        return repository.findById(toDoId)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(new ResponseEntity<>("To do with id" + toDoId + " does not exist", HttpStatus.NOT_FOUND));
     }
 
     @DeleteMapping(value = "/to-do/{toDoId}", produces = {
             MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_XML_VALUE})
-    public ResponseEntity<?> deleteToDo(@PathVariable String toDoId) {
-        Optional<ToDo> existingToDo = repository.findById(toDoId);
-
-        if (existingToDo.isEmpty()) {
-            return new ResponseEntity<>("To do with id" + toDoId + " does not exist", HttpStatus.NOT_FOUND);
-        }
-
-        repository.delete(existingToDo.get());
-        return ResponseEntity.noContent().build();
+    public Mono<ResponseEntity<Void>> deleteToDo(@PathVariable String toDoId) {
+        return repository.findById(toDoId)
+                .map(result -> ResponseEntity.ok().<Void>build())
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @PostMapping(value = "/to-do", produces = {
             MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_XML_VALUE})
-    public ResponseEntity<ToDo> createToDo(@Valid @RequestBody ToDo toDo) {
-        return new ResponseEntity<>(repository.save(toDo), HttpStatus.OK);
+    public Mono<ResponseEntity<ToDo>> createToDo(@Valid @RequestBody ToDo toDo) {
+        return repository.save(toDo)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.internalServerError().build());
     }
 
-    @PutMapping(value = "/to-do", produces = {
-            MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_XML_VALUE})
-    public ResponseEntity<ToDo> updateToDo(@Valid @RequestBody ToDo toDo) {
-        return new ResponseEntity<>(repository.save(toDo), HttpStatus.OK);
-    }
-
-    @PatchMapping(value = "complete-to-do/{toDoId}", produces = {
-            MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_XML_VALUE})
-    public ResponseEntity<?> completeToDO(@PathVariable String toDoId) {
-        Optional<ToDo> existingToDo = repository.findById(toDoId);
-
-        if (existingToDo.isEmpty()) {
-            return new ResponseEntity<>("Such to do does not exist", HttpStatus.NOT_FOUND);
-        }
-
-        existingToDo.get().setCompleted(true);
-        return new ResponseEntity<>(repository.save(existingToDo.get()), HttpStatus.OK);
-    }
+//    @PutMapping(value = "/to-do", produces = {
+//            MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_XML_VALUE})
+//    public ResponseEntity<ToDo> updateToDo(@Valid @RequestBody ToDo toDo) {
+//        return new ResponseEntity<>(repository.save(toDo), HttpStatus.OK);
+//    }
+//
+//    @PatchMapping(value = "complete-to-do/{toDoId}", produces = {
+//            MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_XML_VALUE})
+//    public ResponseEntity<?> completeToDO(@PathVariable String toDoId) {
+//        Optional<ToDo> existingToDo = repository.findById(toDoId);
+//
+//        if (existingToDo.isEmpty()) {
+//            return new ResponseEntity<>("Such to do does not exist", HttpStatus.NOT_FOUND);
+//        }
+//
+//        existingToDo.get().setCompleted(true);
+//        return new ResponseEntity<>(repository.save(existingToDo.get()), HttpStatus.OK);
+//    }
 
     @ExceptionHandler
-    @ResponseStatus(value=HttpStatus.BAD_REQUEST)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ToDoValidationError handleException(Exception e) {
         return new ToDoValidationError(e.getMessage());
     }
--- src/main/java/com/springboot2/repository/ToDoRepository.java
+++ src/main/java/com/springboot2/repository/ToDoRepository.java
@@ -1,9 +1,9 @@
 package com.springboot2.repository;
 
 import com.springboot2.domain.ToDo;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ToDoRepository extends CrudRepository<ToDo,String> {
+public interface ToDoRepository extends ReactiveCrudRepository<ToDo,String> {
 }
